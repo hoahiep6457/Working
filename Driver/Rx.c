fb626fb6143003080611870eb27ca31d97925bc8
unknown <nhuandocan@gmail.com>
2015-10-17 16:25:13 +0700

    done input capture

--- Driver/Rx.c
+++ Driver/Rx.c
@@ -48,14 +48,37 @@ void Rx_Configuration(void)
      The Bothedge is used as active edge,
      The TIM CCR1, CCR2, CCR3 and CCR4 is used to compute the duty cycle and frequency value 
   ------------------------------------------------------------ */
-
-  TIM_ICInitStructure.TIM_Channel = TIM_Channel_1 | TIM_Channel_2 | TIM_Channel_3 | TIM_Channel_4;
+  /* TIM-CH1 Input capture mode */
+  TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
   TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_BothEdge;
   TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
   TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
   TIM_ICInitStructure.TIM_ICFilter = 0x0;
   TIM_ICInit(TIM2, &TIM_ICInitStructure);
 
+	/* TIM-CH2 Input capture mode */
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+  TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_BothEdge;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+  TIM_ICInitStructure.TIM_ICFilter = 0x0;
+  TIM_ICInit(TIM2, &TIM_ICInitStructure);
+
+	/* TIM-CH3 Input capture mode */
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_3;
+  TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_BothEdge;
+  TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+  TIM_ICInitStructure.TIM_ICFilter = 0x0;
+  TIM_ICInit(TIM2, &TIM_ICInitStructure);
+
+	/* TIM-CH4 Input capture mode */
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_4;
+  TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_BothEdge;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+  TIM_ICInitStructure.TIM_ICFilter = 0x0;
+  TIM_ICInit(TIM2, &TIM_ICInitStructure);
   /* Prepare before Calculating Interrupt */
   Rx_Throttle.Number = 0;
   Rx_Pitch.Number = 0;
@@ -65,7 +88,7 @@ void Rx_Configuration(void)
   TIM_Cmd(TIM2, ENABLE);
  
   /* Enable the CC1, CC2, CC3 and CC4 Interrupt Request */
-  TIM_ITConfig(TIM2, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4, ENABLE);
+  TIM_ITConfig(TIM2, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4 , ENABLE);
  }
 /*=====================================================================================================*/
 /*=====================================================================================================*/
@@ -73,89 +96,91 @@ void TIM2_IRQHandler(void)
 {
   /* Interrupt handler for PWM Capture */
   //Calculate Throttle from Tx
-  if (TIM_GetITStatus(TIM2, TIM_IT_CC1)) 
+  if (TIM_GetITStatus(TIM2, TIM_IT_CC1) == SET) 
   {
     /* Clear pending bit */
     TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
-    Calculate_Rx(Rx_Throttle, TIM2->CCR1, GPIO_Pin_0);
+    Calculate_Rx(&Rx_Throttle, TIM2->CCR1, GPIO_Pin_0);
   }
-  if (TIM_GetITStatus(TIM2, TIM_IT_CC2)) 
+  if (TIM_GetITStatus(TIM2, TIM_IT_CC2) == SET) 
   {
     /* Clear pending bit */
     TIM_ClearITPendingBit(TIM2, TIM_IT_CC2);
-    Calculate_Rx(Rx_Pitch, TIM->CCR2, GPIO_Pin_1);
+    Calculate_Rx(&Rx_Pitch, TIM2->CCR2, GPIO_Pin_1);
   }
-  if (TIM_GetITStatus(TIM2, TIM_IT_CC3)) 
+  if (TIM_GetITStatus(TIM2, TIM_IT_CC3) == SET) 
   {
     /* Clear pending bit */
     TIM_ClearITPendingBit(TIM2, TIM_IT_CC3);
-    Calculate_Rx(Rx_Roll, TIM2->CCR3, GPIO_Pin_2);
+    Calculate_Rx(&Rx_Roll, TIM2->CCR3, GPIO_Pin_2);
   }
-  if (TIM_GetITStatus(TIM2, TIM_IT_CC4)) 
+  if (TIM_GetITStatus(TIM2, TIM_IT_CC4) == SET) 
   {
     /* Clear pending bit */
     TIM_ClearITPendingBit(TIM2, TIM_IT_CC4);
-    Calculate_Rx(Rx_Yaw, TIM2->CCR4, GPIO_Pin_3);
+    Calculate_Rx(&Rx_Yaw, TIM2->CCR4, GPIO_Pin_3);
   }
   
 }
 /*=====================================================================================================*/
 /*=====================================================================================================*/
-void Calculate_Rx(Rx_t Rx, uint32_t *CCRx, uint16_t GPIO_Pin_x)
+void Calculate_Rx(Rx_t *Rx, uint32_t CCRx, uint16_t GPIO_Pin_x)
 {
   if ((GPIOA->IDR & GPIO_Pin_x) != 0)
     {
       //Rising
-      if (Rx.Number == 0)
+      if (Rx->Number == 0)
       {
         /* Capture Timer */
-        Rx.Rising = *CCRx;
-        Rx.Number++;
+        Rx->Rising = CCRx;
+        Rx->Number++;
       }
     }
     if ((GPIOA->IDR & GPIO_Pin_x) == 0)
     {
       //Falling 
-      if (Rx.Number == 1)
+      if (Rx->Number == 1)
       {
         /* Capture Timer */
-        Rx.Falling = *CCRx;
+        Rx->Falling = CCRx;
         //Calculate DutyCycle
-        if (Rx.Falling > Rx.Rising)
+        if (Rx->Falling > Rx->Rising)
         {
-          Rx.Highmeasure = Rx.Falling - Rx.Rising;
+          Rx->Highmeasure = Rx->Falling - Rx->Rising;
         }
-        else if (Rx.Falling < Rx.Rising)
+        else if (Rx->Falling < Rx->Rising)
         {
-          Rx.Highmeasure = 0xFFFFFFFF + Rx.Falling - Rx.Rising;
+          Rx->Highmeasure = 0xFFFFFFFF + Rx->Falling - Rx->Rising;
         }
-        else Rx.Highmeasure = 0;
+        else Rx->Highmeasure = 0;
 
-        Rx.NumCCR = TIME_TIM2 / (CLOCK_TIM2 / Rx.Highmeasure);
-        Rx.Number++;
+        Rx->NumCCR = TIME_TIM2 / (CLOCK_TIM2 / Rx->Highmeasure);
+        Rx->Number++;
       } 
     }
     if ((GPIOA->IDR & GPIO_Pin_x) != 0)
     {
       //Rising
-      if (Rx.Number == 2)
+      if (Rx->Number == 2)
       {
         /* Capture Timer */
-        Rx.PreRising = *CCRx;
+        Rx->PreRising = CCRx;
         //Calculate Freqcency
-        if (Rx.PreRising > Rx.Rising)
+        if (Rx->PreRising > Rx->Rising)
         {
-          Rx.Periodmeasure = Rx.PreRising - Rx.Rising;
+          Rx->Periodmeasure = Rx->PreRising - Rx->Rising;
         }
-        else if (Rx.PreRising < Rx.Rising)
+        else if (Rx->PreRising < Rx->Rising)
         {
-          Rx.Periodmeasure = 0xFFFFFFFF + Rx.PreRising - Rx.Rising;
+          Rx->Periodmeasure = 0xFFFFFFFF + Rx->PreRising - Rx->Rising;
         }
-        else Rx.Periodmeasure = 0;
-        Rx.Freqcency = CLOCK_TIM2 / Rx.Periodmeasure;
-        Rx.Number = 0;
+        else Rx->Periodmeasure = 0;
+        Rx->Frequency = CLOCK_TIM2 / Rx->Periodmeasure;
+        Rx->Number = 0;
       }
     }
 }
+/*=====================================================================================================*/
+/*=====================================================================================================*/
 /*=====================================================================================================*/
 /*=====================================================================================================*/
